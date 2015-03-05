bf5f136a6cf27527cce78168457cd37a8bf48c53
chrismcclune <chrismcclune@Chriss-MacBook-Air.local>
2015-03-05 11:51:47 +0000

    New Pages added
    All but the Question Pages have been added

---
+++ QuizForKids/QuizForKids/src/com/example/quizforkids/LevelSelect5to7.java
@@ -0,0 +1,13 @@
+package com.example.quizforkids;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LevelSelect5to7 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_level_select5to7);
+	}
+}
